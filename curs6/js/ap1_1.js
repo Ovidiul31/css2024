44e904a50358c5a868a263a94a80728794722385
Ovidiu Brasov <ovidiubrasov@192-168-0-120.rdsnet.ro>
2024-04-25 20:03:47 +0300

    all

---
+++ curs6/js/ap1_1.js
@@ -0,0 +1,12 @@
+export function add(a,b){
+    let suma = a + b;
+    return suma;
+}
+export function dif(a,b){
+    let diff = a - b;
+    return diff;
+}
+export function inm(a,b){
+    let prod = a*b;
+    return prod;
+}
