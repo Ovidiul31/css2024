d39e50f6ee4eb92ffe54bbcb421e222176a73ceb
Ovidiu Brasov <ovidiubrasov@192-168-0-120.rdsnet.ro>
2024-04-29 21:47:37 +0300

    all

--- curs4/js/ap2.js
+++
@@ -1,8 +0,0 @@
-function creatediv(n){
-    for(var h=0; h<n ;h++){
-        var div = document.createElement('div');
-        div.className='box';
-        document.getElementById("wrapper").appendChild(div);
-    }
-}
-creatediv(72);
--- curs6/js/calculator.js
+++
@@ -1,6 +0,0 @@
-import {add, dif, inm} from "./ap1_1.js"
-let x = 6;
-let y = 4;
-console.info(add(x,y))
-console.info(dif(x,y))
-console.info(inm(x,y))
